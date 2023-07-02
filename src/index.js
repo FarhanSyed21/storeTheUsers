0271b2020cb33e23ed3b4d3fb8881f5f6ceec710
Syed Farhan <100605909+FarhanSyed21@users.noreply.github.com>
2023-07-02 11:02:00 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// const showDetails = document.getElementById("submit");
+
+// showDetails.addEventListener("click", addItems);
+
+// function addItems(event) {
+//   event.preventDefault();
+
+//   const firstName = document.getElementById("firstName").value;
+//   const lastName = document.getElementById("lastName").value;
+//   const email = document.getElementById("email").value;
+//   const number = document.getElementById("number").value;
+
+//   const formData = {
+//     firstName,
+//     lastName,
+//     email,
+//     number
+//   };
+
+//   localStorage.setItem("formData", JSON.stringify(formData));
+
+//   // localStorage.setItem("First Name: ", firstName);
+//   // localStorage.setItem("Last Name: ", lastName);
+//   // localStorage.setItem("Email: ", email);
+//   // localStorage.setItem("Number: ", number);
+
+//   console.log(localStorage);
+// }
